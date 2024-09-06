2a1a8b7136d548a46a152168a729648319d37743
Pedro <pandrade@btcreditos.com.br>
2024-09-06 17:53:01 -0300

    Az login 3

--- __main__.py
+++ __main__.py
@@ -1,8 +1,9 @@
 import pulumi
 import pulumi_azure_native as azure_native
 
-# Exemplo de criação de um grupo de recursos no Azure
-resource_group = azure_native.resources.ResourceGroup("app-resource-group")
 
-# Exporta o nome do grupo de recursos como uma saída
+resource_group = azure_native.resources.ResourceGroup(
+    "app-resource-group",
+    location="Brazil South"  # Localização no Brasil
+)
 pulumi.export("resource_group_name", resource_group.name)
